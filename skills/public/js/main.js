4b42aaf6160b0e7c854a0ce2eedb9931ac707d70
etxeg <etxeetxe1019@gmail.com>
2024-11-04 19:21:36 +0100

    svg ikonoak dinamikoki sortu(jarraipena bukatu gabe)

--- skills/public/js/main.js
+++ skills/public/js/main.js
@@ -1,15 +1,22 @@
-<div class="svg-wrapper" data-id="1" data-custom="false">
-<svg width="100" height="100" viewBox="0 0 100 100">
-<polygon points="50,5 95,27.5 95,72.5 50,95 5,72.5 5,27.5"
-class="hexagon"/>
-<text x="50%" y="20%" text-anchor="middle" fill="black" fontsize="10">
-<tspan x="50%" dy="1.2em" font-weight="bold">Light up an</
-tspan>
-<tspan x="50%" dy="1.2em" font-weight="bold">LED for the
-first </tspan>
-<tspan x="50%" dy="1.2em" font-weight="bold">time</tspan>
-</text>
-<image x="35%" y="60%" width="30" height="30" href="../../
-web_skill_trees_resources/svg/electronics_icons/icon1.svg"/>
-</svg>
-</div>
+fetch("data.json")
+    .then(response => response.json())
+    .then(icons => {
+        const iconsContainer = document.querySelector(".svg-container");
+
+        icons.forEach((icon  => {
+            //wrapper for each icon
+            const svgWrapper = document.createElement("div");
+            svgWrapper.setAttribute("class", "svg-wrapper");
+            svgWrapper.setAttribute("data-id", icon.id);
+            svgWrapper.setAttribute("data-custom", "false");
+
+            //svg element
+            const svg = document.createElement("svg");
+            svg.setAttribute("width", "100");  
+            svg.setAttribute("height", "100");
+            svg.setAttribute("viewBox", "0 0 100 100");
+
+            //hexagon
+            const hexagon = document.createElement("polygon");
+            hexagon.setAttribute("points", "50,5 95,27.5 95,72.5 50,95 5,72.5 5,27.5");
+
